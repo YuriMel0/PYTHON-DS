90ac5283c652901c4dc56da461c71761fea99aaa
Yuri Mel0 <meloy316@gmail.com>
2022-09-22 21:11:14 -0300

    iniciando repositorio e adicionando exemplos iniciais

---
+++ conhecendo-linguagem/tipos_de_dados.py
@@ -0,0 +1,16 @@
+def soma(num1, num2):
+    return num1 + num2
+
+def main():
+    resultado = soma(1, 4)
+    print(resultado)
+
+    resultado = soma(1.5, 4.3)
+    print(resultado)
+
+    print("Exemplo string")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ conhecendo-linguagem/variaveis_e_constantes.py
@@ -0,0 +1,5 @@
+STATEs_BRAZILIAN = ["SP", "RJ", "PB", "ES", "BA"] #exemplo de convenção para constantes
+
+nomeCliente = "João Pessoa" #exemplo variavel simples
+
+primeirosNaturais = [1, 2, 3, 4, 5, 6, 7] #exemplo de lista em python
