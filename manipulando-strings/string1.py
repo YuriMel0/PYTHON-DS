46eb9c6e9f11d8ee542e5adb8f1b11a88879e26c
Yuri Mel0 <meloy316@gmail.com>
2022-09-26 16:42:54 -0300

    adicionando exemplo com string em python

---
+++ manipulando-strings/string1.py
@@ -0,0 +1,14 @@
+from ctypes.wintypes import PINT
+
+
+nome = "ShaZaM"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "  Ola Mundo     !"
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
---
+++ manipulando-strings/string2.py
@@ -0,0 +1,9 @@
+nome = "Shazam paraguai alibaba core"
+
+print(nome[0])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:10])
+print(nome[10:10:2])
+print(nome[:])
+print(nome[::-1])
