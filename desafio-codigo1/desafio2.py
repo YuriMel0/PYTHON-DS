b9f6f970123c79969882a584d1c2dff272c20771
Yuri Mel0 <meloy316@gmail.com>
2022-09-26 17:18:52 -0300

    entregando desafio2_OK

---
+++ desafio-codigo1/desafio2.py
@@ -0,0 +1,8 @@
+valores = input().split()
+
+cachorroQuentes = int(valores[0])
+participantes = int(valores[1])
+
+mediaConsumidos = cachorroQuentes / participantes
+
+print(f"{mediaConsumidos:.2f}")
